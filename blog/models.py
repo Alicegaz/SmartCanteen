6c6fa73b4bca8461a608f3c4a4b46764d815db3a
Alicegaz <gazizullina2010@yandex.ru>
2016-05-23 08:09:36 +0300

    Models.py

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -11,11 +16,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def __init__(self):
-        author = models.ForeignKey('auth.User')
-        title = models.CharField(max_length=200)
-        text = models.TextField()
-        created_date = models.DateTimeField(default=timezone.now)
-        published_date = models.DateTimeField(blank=True, null=True)
 
 # #Create your models here.
