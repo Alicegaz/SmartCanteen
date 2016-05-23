d355ad47064a98f20c1218117ef80e61563afa5c
Alicegaz <gazizullina2010@yandex.ru>
2016-05-23 17:07:01 +0300

    s

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 
 def post_list(request):
@@ -12,3 +13,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
