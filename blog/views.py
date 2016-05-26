82abac7ec455288b2a513caab35c7e37f9dfdee7
Alicegaz <gazizullina2010@yandex.ru>
2016-05-26 09:07:07 +0300

    changes

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 
 def post_list(request):
@@ -16,5 +17,14 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
--- mysite/settings.py
+++ mysite/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Moscow'
-MEDIA_ROOT = '/home/SmartFoods/mysite/media/'
+MEDIA_ROOT = '/home/djangogirls1/mysite/media/'
 
 MEDIA_URL = '/media/'
 
