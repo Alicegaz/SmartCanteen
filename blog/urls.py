60b92287f60fdc0f0f98b727e0eb79e1df2c8aa9
Alicegaz <gazizullina2010@yandex.ru>
2016-05-28 08:29:16 +0300

    base deleted

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new')
-    url(r'^post/(?P<pk>\d+)/images/$', view.post)
+    # url(r'^post/(?P<pk>\d+)/images/$', view.post)
 ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
