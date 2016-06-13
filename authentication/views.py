7c55d97109b9543c5ba0c7fcea50afb2f32abd71
valt25 <Goamfs44zi>
2016-06-13 16:53:25 +0300

    debug user.length

--- authentication/views.py
+++ authentication/views.py
@@ -1,110 +1,5 @@
-"""import status as status
-from django.http import response
-from requests import Response
-from rest_framework import authentication
-
-from rest_framework import permissions, viewsets
-
-from authentication.models import Account
-from authentication.permissions import IsAccountOwner
-from authentication.serializers import AccountSerializer
-import json
-from django.contrib.auth import authenticate, login
-from rest_framework import status, views
-# import codecs
-from rest_framework.response import Response
-from django.contrib.auth import logout
-from rest_framework import permissions
-from django.shortcuts import render_to_response, redirect
-from django.contrib import auth
-from django.core.context_processors import csrf
-
-
-def login(request):
-    args = {}
-    args.update(csrf(request))
-    if request.POST:
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        account = authentication.authenticate(username=username, password=password)
-        if account is not None:
-            authentication.login(request, account)
-            return redirect('/')
-        else:
-            args['login_error'] = "Пользователь не найден"
-            return render_to_response('login.html', args)
-    else:
-        return render_to_response('login.html', args)
-
-
-def logout(request):
-    authentication.logout(request)
-    return redirect("/post_list.html")
-#class AccountViewSet(viewsets.ModelViewSet):
-    lookup_field = 'username'
-    queryset = Account.objects.all()
-    serializer_class = AccountSerializer
-
-    def get_permissions(self):
-        if self.request.method in permissions.SAFE_METHODS:
-            return (permissions.AllowAny(),)
-
-        if self.request.method == 'POST':
-            return (permissions.AllowAny(),)
-
-        return (permissions.IsAuthenticated(), IsAccountOwner(),)
-
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-
-        if serializer.is_valid():
-            Account.objects.create_user(**serializer.validated_data)
-
-            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LoginView(views.APIView):
-    def post(self, request, format=None):
-        # reader = codecs.getreader("utf-8")
-        data = json.loads(request.body)
-
-        email = data.get('email', None)
-        password = data.get('password', None)
-
-        account = authenticate(email=email, password=password)
-
-        if account is not None:
-            if account.is_active:
-                login(request, account)
-
-                serialized = AccountSerializer(account)
-
-                return Response(serialized.data)
-            else:
-                return Response({
-                    'status': 'Unauthorized',
-                    'message': 'This account has been disabled.'
-                }, status=status.HTTP_401_UNAUTHORIZED)
-        else:
-            return Response({
-                'status': 'Unauthorized',
-                'message': 'Username/password combination invalid.'
-            }, status=status.HTTP_401_UNAUTHORIZED)
-
-
-class LogoutView(views.APIView):
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def post(self, request, format=None):
-        logout(request)
-
-        return Response({}, status.HTTP_204_NO_CONTENT)
-        """
-
 from django.shortcuts import render_to_response, redirect, render
-from django.contrib.auth.models import User
+from authentication.models import User
 from django.template.context_processors import csrf
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from authentication import forms
@@ -149,8 +44,10 @@ def register(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            is_staff = form.cleaned_data.get('is_staff')
-            user = User.objects.create_user(username=username, password=password,is_staff=is_staff)
+            is_admin = None
+            is_cashier = None
+            user = User.objects.create_user(username=username, password=password)
+            user.user_permissions.add()
             user.save()
             return redirect('/')
     return render(request, 'register.html', {'form': form})
