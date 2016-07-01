75002a14511a02128e5c1405ced913679aceea33
Alicegaz <gazizullina2010@yandex.ru>
2016-07-01 16:29:48 +0300

    menu changed position fixed

--- static/javascripts/menu.js
+++ static/javascripts/menu.js
@@ -1,36 +1,38 @@
-(function(){  // анонимная функция (function(){ })(), чтобы переменные "a" и "b" не стали глобальными
-var a = document.querySelector('#aside1'), b = null;  // селектор блока, который нужно закрепить
-window.addEventListener('scroll', Ascroll, false);
-document.body.addEventListener('scroll', Ascroll, false);  // если у html и body высота равна 100%
-function Ascroll() {
-  if (b == null) {  // добавить потомка-обёртку, чтобы убрать зависимость с соседями
-    var Sa = getComputedStyle(a, ''), s = '';
-    for (var i = 0; i < Sa.length; i++) {  // перечислить стили CSS, которые нужно скопировать с родителя
-      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
-      }
-    }
-    b = document.createElement('div');  // создать потомка
-    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-    a.insertBefore(b, a.firstChild);  // поместить потомка в цепляющийся блок первым
-    var l = a.childNodes.length;
-    for (var i = 1; i < l; i++) {  // переместить во вновь созданного потомка всех остальных потомков (итого: создан потомок-обёртка, внутри которого по прежнему работают скрипты)
-      b.appendChild(a.childNodes[1]);
-    }
-    a.style.height = b.getBoundingClientRect().height + 'px';  // если под скользящим элементом есть другие блоки, можно своё значение
-    a.style.padding = '0';
-    a.style.border = '0';  // если элементу присвоен padding или border
-  }
-  if (a.getBoundingClientRect().top <= 0) { // elem.getBoundingClientRect() возвращает в px координаты элемента относительно верхнего левого угла области просмотра окна браузера
-    b.className = 'sticky';
-  } else {
-    b.className = '';
-  }
-  window.addEventListener('resize', function() {
-    a.children[0].style.width = getComputedStyle(a, '').width
-  }, false);  // если изменить размер окна браузера, измениться ширина элемента
-}
-})()
-/**
- * Created by Alice on 01.07.2016.
- */
+        var m1 = 80;
+        var m2 = 8;
+        function getScrollTop(){
+            var scrOfY = 0;
+            if( typeof( window.pageYOffset ) == "number" ) {
+                scrOfY = window.pageYOffset;
+            }
+            else if( document.body && (document.body.scrollLeft || document.body.scrollTop )){
+                scrOfY = document.body.scrollTop;
+            } else if(document.documentElement && (document.documentElement.scrollLeft || document.documentElement.scrollTop )){
+                scrOfY = document.documentElement.scrollTop;
+            }
+            return scrOfY;
+        }
+        function marginMenuTop()
+        {
+            var top = getScrollTop();
+            var s= document.getElementById("socializ");
+            if (top+m2 < m1){
+                s.style.top = (m1-top) + "px";
+            }
+            else{
+                s.style.top = m2 + "px";
+            }
+        }
+        function setMenuPosition(){
+            if(typeof window.addEventListener != "undefined"){
+                window.addEventListener("scroll", marginMenuTop, false);
+            }
+            else if(typeof window.attachEvent != "undefined"){
+                window.attachEvent("onscroll", marginMenuTop);
+            }
+        }
+        if(typeof window.addEventListener != "undefined"){
+            window.addEventListener("load", setMenuPosition, false);
+        } else if(typeof window.attachEvent != "undefined"){
+            window. attachEvent("onload", setMenuPosition);
+        }
