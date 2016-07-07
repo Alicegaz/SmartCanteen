4bab7955c1a37ce534ffd8c3c02ef681548dbb72
valt25 <Goamfs44zi>
2016-07-07 20:13:16 +0300

    request issues

--- common/permission.py
+++ common/permission.py
@@ -4,13 +4,14 @@ from django.contrib.auth import authenticate
 
 def have_permission(request, role=None):
     user = request.user
-    if user is AnonymousUser:
+    if isinstance(user, AnonymousUser):
         try:
             username = request.POST.get('username')
             password = request.POST.get('password')
-        except:
+        except Exception:
             return False
-        user = authenticate(username, password)
+        user = authenticate(username=username, password=password)
+        print(user)
     if role is not None:
         if user.user_permissions.contain(role):
             return user
